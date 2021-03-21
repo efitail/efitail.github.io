415d5d24d12954a83c8ec8957ab36a2953514675
efitail <efitailvlg@gmail.com>
2021-03-21 13:36:58 +0300

    - A lot of changes.

---
+++ src/js/components/pages/landing/partials/contact-form.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+    import Component from "vue-class-component";
+
+    @Component({
+        components: {}
+    })
+    export default class ContactFormComponent extends Vue {
+    }
+</script>
+
+<style scoped>
+
+</style>
